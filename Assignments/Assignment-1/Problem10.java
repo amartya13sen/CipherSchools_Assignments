b41a648291cc35a94cda17a9f2d55a2de99ae503
s1ddhant-tyagi <53624966+s1ddhant-tyagi@users.noreply.github.com>
2021-02-17 21:06:04 +0530

    trapping water

---
+++ Assignments/Assignment-1/Problem10.java
@@ -0,0 +1,33 @@
+class Problem10
+{
+    public static int maxWater(int[] arr, int n) 
+    {
+        int returnResult = 0;
+        for(int i = 1; i < n - 1; i++)
+        {
+            int left = arr[i];
+            for(int j = 0; j < i; j++)
+            {
+                left = Math.max(left, arr[j]);
+            }
+            int right = arr[i];
+            for(int j = i + 1; j < n; j++)
+            {
+                right = Math.max(right, arr[j]);
+            }
+            returnResult += Math.min(left, right) - arr[i];
+        }
+        return returnResult;
+    }
+     
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0; i<n; i++)
+            arr[i]=sc.nextInt();
+        System.out.print(maxWater(arr,n));
+    }
+    }
+     
